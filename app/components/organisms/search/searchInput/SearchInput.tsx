92ed6d46e6407ca9c69178f04c7e4fb8d456ea1c
Olivier Rocancourt <olivier.rocancourt@nuktek.net>
2023-12-15 16:01:46 +0100

    feat: Added autocomplete=off on search input

--- app/components/organisms/search/searchInput/SearchInput.tsx
+++ app/components/organisms/search/searchInput/SearchInput.tsx
@@ -11,7 +11,7 @@ type SearchInputProps = {
 
 export default function SearchInput({ query, onChange }: SearchInputProps) {
   const [value, setValue] = useState(query)
-  const debouncedValue = useDebounce(value, 500)
+  const debouncedValue = useDebounce(value, 250)
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value)
@@ -28,6 +28,7 @@ export default function SearchInput({ query, onChange }: SearchInputProps) {
       </div>
 
       <input
+        autoComplete="off"
         type="search"
         id="default-search"
         className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
